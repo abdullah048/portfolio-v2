50c7f088a22b6f9e52a7996cb6c7e40332adb861
IB1148981 <103644657+IB1148981@users.noreply.github.com>
2022-09-21 21:40:01 +0500

    Added tailwindcss into the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
